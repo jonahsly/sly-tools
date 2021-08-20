d6bb83f12a56a94dfa513cae1a504685cbd063da
TheShona <slyjonatan@gmail.com>
2021-08-20 16:11:35 -0300

    complementos

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
     const month = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'setiembre', 'octubre', 'noviembre', 'diciembre']
     for (let index = 0; index < 12; index++) {
       if (tmda.mes.toLowerCase() == month[index]) {
-        return porcAdia[index];
+        return porcAmes[index];
       }
     }
   }
@@ -65,7 +65,8 @@ function App() {
   console.log(tmda);
 
   return (
-    <div className="App">
+    <React.Fragment>
+      <div className="App">
       <header className="App-header">
         <h1>
           Sly Tools
@@ -122,6 +123,46 @@ function App() {
         </div>
       </header>
     </div>
+    <div className="h-screen w-full">
+  <div className="bg-gray-800 h-screen mx-auto max-w-md">
+    <div className="p-12">
+      <p className="text-5xl pt-10 text-yellow-500 font-bold">
+        Welcome <br />
+        Back
+      </p>
+      <p className="text-xl py-3 text-gray-400 font-semibold">Sign in to continue</p>
+    </div>
+    <div className="mx-12 p-3 rounded-xl shadow-sm bg-gray-900">
+      <div className="p-3 mx-6 border-b border-gray-500">
+        <input placeholder="Phone Number" className="bg-transparent text-yellow-500 w-full focus:outline-none focus:rang" type="tel" />
+      </div>
+
+      <div className="p-3 mx-6 flex border-b border-gray-500">
+        <input placeholder="Password" className="bg-transparent text-yellow-500 focus:outline-none focus:rang w-full" type="password" />
+        <div className="w-auto text-yellow-500">eyes</div>
+      </div>
+    </div>
+    <div className="mx-12 p-3 justify-between flex">
+      <div className="flex">
+        <div className="relative inline-block w-12 mr-2 align-middle select-none transition duration-200 ease-in">
+          <input type="checkbox" name="toggle" id="toggle" className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-gray-800 border-4 appearance-none cursor-pointer" />
+          <label htmlFor="toggle" className="toggle-label block overflow-hidden h-6 rounded-full bg-yellow-500 cursor-pointer"></label>
+        </div>
+        <label htmlFor="toggle" className="text-xs text-gray-300 mt-1">remeber me</label>
+      </div>
+      <div className="bg mt-1 text-xs text-gray-300">
+        <a href="">forget password?</a>
+      </div>
+    </div>
+    <div className="w-full p-12">
+      <button className=" bg-yellow-500 p-3 rounded-3xl w-full h-full hover:bg-yellow-600"> Login</button>
+      <p className="mx-auto text-center mt-3 text-gray-400">don't have an account?  <a href="" className="text-md font-semibold">Sign up</a> </p>
+    </div>
+  </div>
+</div>
+
+    </React.Fragment>
+    
   );
 }
 
